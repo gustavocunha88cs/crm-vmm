57cdc7d1f45f725382acbae6b3fb99b08d1f6dd0
VMM Admin <admin@vmm.local>
2026-06-01 10:58:23 -0300

    Configure allowedDevOrigins for AWS public IP

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ["18.231.142.114", "localhost:3000"],
   devIndicators: {
     appIsrStatus: false,
     buildActivity: false,
